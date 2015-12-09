e53fd8d661cc0b66e8fd649dd5d6f429e97cfa22
Emad Elmogy <emadelmogy619@gmail.com>
2015-12-09 21:55:31 +0200

    print the conversion rate

--- currency_convert.py
+++ currency_convert.py
@@ -1,19 +1,19 @@
 import requests
 
-a = raw_input('Enter currency to convert from?')
+a = raw_input('Enter currency to convert from ?')
 a = a.upper()
 
-b = raw_input('Enter currency to convert to?')
+b = raw_input('Enter currency to convert to ?')
 b = b.upper()
 
-c = float(raw_input('Enter value to convert?'))
+c = float(raw_input('Enter value to convert ?'))
 
 url = ('https://currency-api.appspot.com/api/%s/%s.json') % (a, b)
 print url
 
 r = requests.get(url)
 rate= r.json()['rate']
-print "rate => "+str(rate)
+print "rate is => "+str(rate)
 
 value= c*float(r.json()['rate'])
-print "value => "+str(value)
+print "value is => "+str(value)
